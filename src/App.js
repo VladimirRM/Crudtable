61f5b017c19044b0dfc845bd2a0a19fc895d6c90
Vladimir RM <shvdrummer@gmail.com>
2022-10-25 08:38:37 +0300

    create css

--- src/App.js
+++ src/App.js
@@ -1,59 +1,67 @@
-import React, { useState} from 'react';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 
 const initialValues = {
-  userName:'',
-  userSurname:'',
-  userSalary:''
-}
+  userName: "",
+  userSurname: "",
+  userSalary: "",
+};
 
 function App() {
-const [userData,setUserData]= useState(initialValues)
+  const [userData, setUserData] = useState(initialValues);
 
-console.log('userData', userData);
+  console.log("userData", userData);
 
   return (
-    <div className='wrapper'>
-     <div  className="wrapper-content">
-      <div className='table-data'>
-        <table>
-          <th>#</th>
-          <th>User Name</th>
-          <th>User SurName</th>
-          <th>User Salary</th>
-          <th>Actions</th>
-          <tbody></tbody>
-        </table>
+    <div className="wrapper">
+      <div className="wrapper-content">
+        <div className="table-data">
+          <table>
+            <th>#</th>
+            <th>User Name</th>
+            <th>User SurName</th>
+            <th>User Salary</th>
+            <th>Actions</th>
+            <tbody></tbody>
+          </table>
+        </div>
+        <div>
+          <form>
+            <input
+              placeholder="Write your name"
+              onChange={(e) =>
+                setUserData((prevState) => ({
+                  ...prevState,
+                  userName: e.target.value,
+                }))
+              }
+            />
+            <input
+              placeholder="Write surname"
+              onChange={(e) =>
+                setUserData((prevState) => ({
+                  ...prevState,
+                  userSurName: e.target.value,
+                }))
+              }
+            />
+            <input
+              placeholder="Write your salary"
+              onChange={(e) =>
+                setUserData((prevState) => ({
+                  ...prevState,
+                  userSalary: e.target.value,
+                }))
+              }
+            />
+
+            <div className="buttons-wrapper">
+              <button type="reset">Clean</button>
+              <button type="submit">Add</button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div>
-        <form>
-          <input placeholder='Write your name' onChange={(e)=>setUserData((prevState)=>({
-            ...prevState,
-            userName:e.target.value
-          }))}/>
-          <input placeholder='Write your name' onChange={(e)=>setUserData((prevState)=>({
-            ...prevState,
-            userName:e.target.value
-          }))}/>
-          <input placeholder='Write your name' onChange={(e)=>setUserData((prevState)=>({
-            ...prevState,
-            userName:e.target.value
-          }))}/>
-          <input placeholder='Write surname' />
-          <input placeholder='Write your salary' />
-          <div className='buttons-wrapper'>
-            <button type='reset'>Clean</button>
-            <button type='submit'>Add</button>
-          </div>
-        </form>
-
-
-
-
-      </div>
-     </div>
-     
     </div>
   );
 }
