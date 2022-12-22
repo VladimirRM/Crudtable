bd9db24286d58bf6d739cea623d28ec1c05e5583
Vladimir RM <shvdrummer@gmail.com>
2022-12-22 16:56:58 +0200

    add th gor data users

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <table>
+            <th>#</th>
+            <th>User Name</th>
+            <th>User SurName</th>
+            <th>User Salary</th>
+            <th>Actions</th>
+
+
+            <tbody>
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
